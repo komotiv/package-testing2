8530658015ad9a5a556977532346cefbbd7007a2
Jan Jezabek <jezabek@gmail.com>
2025-02-25 22:41:10 +0000

    Import stuffs

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="komotiv_package_testing",
+    version="0.1.0",
+    author="Komotiv Inc.",
+    author_email="contact@komotiv.com",
+    description="Komotiv package test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["tests", "docs"]),
+    install_requires=[
+        "openai",
+        "https://github.com/komotiv/package-testing2",
+    ],
+)
